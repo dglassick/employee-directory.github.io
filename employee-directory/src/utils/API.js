eba79bdb07875954dacd5eb960d8b1f5e9327ad7
Derek Glassick <derek.glassick@gmail.com>
2020-07-01 18:31:59 -0700

    moved everything to the main folder

--- employee-directory/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default {
-    randomEmployees : function (query) {
-        return axios.get(
-            "https://randomuser.me/api/?results=20&nat=us"
-        )
-    }
-}
